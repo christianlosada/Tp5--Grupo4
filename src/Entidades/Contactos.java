343327df237f30d1ccee9cbef6aac8bebd7e3803
christianlosada <christianalejandrolosada@gmail.com>
2024-09-05 18:09:19 -0300

    Initial commit

---
+++ src/Entidades/Contactos.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Netbook
+ */
+public class Contactos {
+    //Un directorio telefónico posee una lista de Contactos de los que interesa conocer su
+//DNI, nombre, apellido, ciudad y dirección. Un contacto se empareja con un teléfono tipo Long
+    
+    int dni;
+    String nombre, apellido, ciudad, direccion;
+    Long telefono;
+
+    public Contactos(int dni, String nombre, String apellido, String ciudad, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+       
+    }
+
+    @Override
+    public String toString() {
+        return ""+nombre+apellido;
+    }
+    
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    
+    
+    
+}
---
+++ src/Entidades/Directorio.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Netbook
+ */
+public class Directorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    // TODO code application logic here
+    private TreeMap<Long, Contactos> directorio = new TreeMap();
+
+    public TreeMap<Long, Contactos> getDirectorio() {
+        return directorio;
+    }
+
+    public void setDirectorio(TreeMap<Long, Contactos> directorio) {
+        this.directorio = directorio;
+    }
+
+    @Override
+    public String toString() {
+        return "Directorio{" + "directorio=" + directorio + '}';
+    }
+    
+
+// A. agregarContacto() que permite registrar un nuevo contacto con su respectivo nro. de
+//teléfono. Siendo el nro. del teléfono la clave del mismo.
+    public void agregarContacto(Long telefono, Contactos contacto) {
+        
+    }
+//B. buscarContacto() que en base al nro. de teléfono retorna el Contacto asociado al mismo.
+
+    public String buscarUnContacto(Long telefono) {
+        for (Long t : directorio.keySet()) {
+            if (t == telefono) {
+                return directorio.ceilingEntry(telefono).toString();
+            }
+
+        }
+        return "contacto no encontrado";
+    }
+
+//C. buscarTeléfono() que en base a un apellido nos devuelve un Set<Long> con los números
+//de teléfono asociados a dicho apellido.
+    public Set<Long> buscarTelefono(String apellidoBuscado) {
+        Set<Long> lista = null;
+        for (Contactos p : directorio.values()) {
+
+            if (p.getApellido().equalsIgnoreCase(apellidoBuscado)) {
+
+                lista.add(p.getTelefono());
+
+            }
+
+        }
+        return lista;
+
+    }
+
+//D. buscarContactos() que en base a una ciudad nos devuelve un ArrayList con los
+//Contactos asociados a dicha ciudad.
+    public ArrayList buscarVariosContactos(String ciudadBuscada) {
+        ArrayList<String> ciudadesEncontradas = new ArrayList();
+        for (Contactos p : directorio.values()) {
+            if (p.getCiudad().equalsIgnoreCase(ciudadBuscada)) {
+                ciudadesEncontradas.add(ciudadBuscada);
+
+            }
+
+        }
+        return ciudadesEncontradas;
+
+    }
+//E. borrarContacto() que en base al número de teléfono elimina el contacto del directorio
+
+    public String borrarContacto(Long telefono) {
+        for (Long t : directorio.keySet()) {
+            if (t == telefono) {
+                directorio.remove(t);
+                return "removido";
+
+            }
+        }
+        return "no encontrado";
+    }
+}
